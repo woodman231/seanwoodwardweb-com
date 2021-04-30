edf1c0a26c8b63584b6728be46937047ab7b7037
Sean Woodward <seanwoodward@live.com>
2021-04-30 15:06:28 -0600

    Adds PDF version of Resume. Adds links to resume in the nav bar. Puts Blog as the last link in the nav bar.

--- components/nav.js
+++ components/nav.js
@@ -10,14 +10,16 @@ export default function MyNav({ preview, children }) {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Link href="/blog" passHref>
-                        <Nav.Link>Blog</Nav.Link>
-                    </Link>
+                    <Nav.Link href="/Sean-Woodward.pdf" target="_blank">Resume (PDF)</Nav.Link>
+                    <Nav.Link href="https://my.indeed.com/p/seanw-z1tlkqi" target="_blank">Resume (Indeed)</Nav.Link>                    
                     <Link href="/blog/projects" passHref>
                         <Nav.Link>Projects</Nav.Link>
                     </Link>
                     <Link href="/blog/achievements-awards" passHref>
                         <Nav.Link>Achievements / Awards</Nav.Link>
+                    </Link>
+                    <Link href="/blog" passHref>
+                        <Nav.Link>Blog</Nav.Link>
                     </Link>
                 </Nav>
             </Navbar.Collapse>
